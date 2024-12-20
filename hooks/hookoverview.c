94c2feac9e58bcb23b59c3b92c136bb2b20ced58
Kharay33455 <shakibkhan276337@gmail.com>
2024-12-20 19:36:54 +0100

    initial

---
+++ hooks/hookoverview.c
@@ -0,0 +1,10 @@
+// A hook is a point in a system where developers can intercept system routines and read them,
+// edit them and even silent them completely.
+
+// A hook procedure is a function that is called to handle the event. It is a callback function
+
+//Hooks slow down systems. Remove as soon as you can.
+
+// A hook chain is a list of pointers to hook procedure functions. They pass a hook from start to end when it is triggered.
+
+//
